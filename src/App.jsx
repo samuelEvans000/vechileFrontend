d4c98fc996ab7bdbdb91f80b97ea2e57a77b8545
Vincent Samuel <chintuvincent@gmail.com>
2024-07-25 23:34:52 +0530

    api changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/api/vehicle");
+        const response = await axios.get("https://vechilebackend.onrender.com/api/vehicle");
         const { defaultLocation, routeData } = response.data;
         setRouteData(routeData);
         setCurrentPosition([
