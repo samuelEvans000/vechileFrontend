7d5d3fdef6438dd9e78779c6efc39acf299e40db
Vincent Samuel <chintuvincent@gmail.com>
2024-07-26 12:56:32 +0530

    optimized

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import "./App.css";
 
 const center = [17.385044, 78.486671];
 
-
 const vehicleIcon = new L.Icon({
   iconUrl: "/assets/vehicle.png",
   iconSize: [40, 40],
@@ -18,31 +17,40 @@ const App = () => {
   const [currentPosition, setCurrentPosition] = useState(center);
   const [isMoving, setIsMoving] = useState(false);
   const [progress, setProgress] = useState(0);
-  const [selectedRoute, setSelectedRoute] = useState("Route 1");
+  const [selectedRoute, setSelectedRoute] = useState("select");
+  const [loading, setLoading] = useState(true); // New state for loading indicator
   const intervalRef = useRef(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("https://vechilebackend.onrender.com/api/vehicle");
-        const { defaultLocation, routeData } = response.data;
-        setRouteData(routeData);
-        setCurrentPosition([
-          defaultLocation.latitude,
-          defaultLocation.longitude,
-        ]);
-        console.log("Fetched route data: ", routeData);
-        console.log("Set current position: ", [
-          defaultLocation.latitude,
-          defaultLocation.longitude,
-        ]);
-      } catch (error) {
-        console.error("Error fetching vehicle data:", error);
-      }
-    };
-
-    fetchData();
+    const storedRoute = localStorage.getItem("selectedRoute");
+    if (storedRoute) {
+      setSelectedRoute(storedRoute);
+      handleRouteChange({ target: { value: storedRoute } }, true);
+    } else {
+      fetchData();
+    }
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://vechilebackend.onrender.com/api/vehicle");
+      const { defaultLocation, routeData } = response.data;
+      setRouteData(routeData);
+      setCurrentPosition([
+        defaultLocation.latitude,
+        defaultLocation.longitude,
+      ]);
+      console.log("Fetched route data: ", routeData);
+      console.log("Set current position: ", [
+        defaultLocation.latitude,
+        defaultLocation.longitude,
+      ]);
+    } catch (error) {
+      console.error("Error fetching vehicle data:", error);
+    } finally {
+      setLoading(false); // Hide loading indicator
+    }
+  };
 
   useEffect(() => {
     if (isMoving && routeData.length > 1) {
@@ -58,7 +66,10 @@ const App = () => {
             routeData[nextIndex].latitude,
             routeData[nextIndex].longitude,
           ]);
-          console.log("Moved vehicle to: ", [routeData[nextIndex].latitude, routeData[nextIndex].longitude]);
+          console.log("Moved vehicle to: ", [
+            routeData[nextIndex].latitude,
+            routeData[nextIndex].longitude,
+          ]);
           setProgress(((nextIndex + 1) / routeData.length) * 100);
         } else {
           clearInterval(intervalRef.current);
@@ -72,21 +83,30 @@ const App = () => {
     return () => clearInterval(intervalRef.current);
   }, [isMoving, routeData, currentPosition]);
 
-  const handleRouteChange = async (e) => {
+  const handleRouteChange = async (e, initialLoad = false) => {
     const routeName = e.target.value;
     setSelectedRoute(routeName);
+    if (!initialLoad) {
+      localStorage.setItem("selectedRoute", routeName);
+    }
     try {
+      setLoading(true); // Show loading indicator
       await axios.post("https://vechilebackend.onrender.com/api/select-route", { routeName });
       const response = await axios.get("https://vechilebackend.onrender.com/api/vehicle");
       const { defaultLocation, routeData } = response.data;
       setRouteData(routeData);
-      setCurrentPosition([defaultLocation.latitude, defaultLocation.longitude]);
+      setCurrentPosition([
+        defaultLocation.latitude,
+        defaultLocation.longitude,
+      ]);
       console.log("Route changed to: ", routeName);
       console.log("Fetched new route data: ", routeData);
       setProgress(0);
       setIsMoving(false);
     } catch (error) {
       console.error("Error updating route:", error);
+    } finally {
+      setLoading(false); // Hide loading indicator
     }
   };
 
@@ -111,58 +131,62 @@ const App = () => {
 
   return (
     <div className="mapContainer">
-      <MapContainer
-        center={currentPosition}
-        zoom={15}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {routeData.length > 0 && (
-          <>
-            <Marker position={currentPosition} icon={vehicleIcon} />
-            <Polyline positions={pathCoordinates} color="red" />
-          </>
-        )}
-      </MapContainer>
-      <div className="container">
-        <select
-          value={selectedRoute}
-          onChange={handleRouteChange}
-        >
-          <option value="Route 1">mehfil restaurant</option>
-          <option value="Route 2">yashodha hospital</option>
-          <option value="Route 3">osmania university</option>
-        </select>
-        <button className="start" onClick={toggleMovement}>
-          <img src={getButtonImageSrc()} alt="Toggle Movement" />
-        </button>
-        <button className="stop" onClick={restartMovement}>
-          <img src="/assets/restart.png" alt="Restart" />
-        </button>
-        <div
-          style={{
-            zIndex: 1000,
-            width: "300px",
-            height: "5px",
-            backgroundColor: "#e0e0e0",
-            borderRadius: "10px",
-            overflow: "hidden",
-            marginTop: "10px",
-          }}
-        >
-          <div
-            style={{
-              width: `${progress}%`,
-              height: "100%",
-              backgroundColor: "rgb(62, 117, 189)",
-              transition: "width 1s ease",
-            }}
-          />
-        </div>
-      </div>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <MapContainer
+            center={currentPosition}
+            zoom={15}
+            style={{ height: "100%", width: "100%" }}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {routeData.length > 0 && (
+              <>
+                <Marker position={currentPosition} icon={vehicleIcon} />
+                <Polyline positions={pathCoordinates} color="red" />
+              </>
+            )}
+          </MapContainer>
+          <div className="container">
+            <select value={selectedRoute} onChange={handleRouteChange}>
+              <option value="select">Select</option>
+              <option value="Route 1">mehfil restaurant</option>
+              <option value="Route 2">yashodha hospital</option>
+              <option value="Route 3">osmania university</option>
+            </select>
+            <button className="start" onClick={toggleMovement}>
+              <img src={getButtonImageSrc()} alt="Toggle Movement" />
+            </button>
+            <button className="stop" onClick={restartMovement}>
+              <img src="/assets/restart.png" alt="Restart" />
+            </button>
+            <div
+              style={{
+                zIndex: 1000,
+                width: "300px",
+                height: "5px",
+                backgroundColor: "#e0e0e0",
+                borderRadius: "10px",
+                overflow: "hidden",
+                marginTop: "10px",
+              }}
+            >
+              <div
+                style={{
+                  width: `${progress}%`,
+                  height: "100%",
+                  backgroundColor: "rgb(62, 117, 189)",
+                  transition: "width 1s ease",
+                }}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
